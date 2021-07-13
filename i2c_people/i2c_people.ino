49d16325499148be46bab83f0f96d4c0a6ca2df5
SANDESH2310 <66778975+SANDESH2310@users.noreply.github.com>
2021-07-13 18:51:36 +0530

    Update i2c_people.ino

--- i2c_people/i2c_people.ino
+++ i2c_people/i2c_people.ino
@@ -10,6 +10,8 @@ Sensor_1_old_value = 0,
 Sensor_2_old_value = 0, 
 Sensor_3_old_value = 0;
 char c[15];
+char temp_char;
+char x[15];
 char v[8];
 void setup() {
   Wire.begin(8);       // join i2c bus (address optional for master)
@@ -17,32 +19,43 @@ void setup() {
 }
 
 void loop() {
-  sensor_read_value(Sensor_1_add, 15);
+  sensor_read_value(Sensor_1_add, 17);
   parsing_val(Sensor_1_add);
-  val_comparsion(Sensor_1_old_value, Sensor_1_new_value);
+  if(val_comparsion(Sensor_1_old_value, Sensor_1_new_value))
+  {
+    Sensor_1_old_value = Sensor_1_new_value;  
+   }
   delay(100);
-  sensor_read_value(Sensor_2_add, 15);
+  sensor_read_value(Sensor_2_add, 17);
   parsing_val(Sensor_2_add);
-  val_comparsion(Sensor_2_old_value, Sensor_2_new_value);
+  if(val_comparsion(Sensor_2_old_value, Sensor_2_new_value))
+  {
+    Sensor_2_old_value = Sensor_2_new_value;
+  }
   delay(100);
-  sensor_read_value(Sensor_3_add, 15);
+  sensor_read_value(Sensor_3_add, 17);
   parsing_val(Sensor_3_add);
-  val_comparsion(Sensor_3_old_value, Sensor_3_new_value);
+  if(val_comparsion(Sensor_3_old_value, Sensor_3_new_value))
+  {
+    Sensor_3_old_value = Sensor_3_new_value;
+  }
   delay(100);
 }
 
 uint8_t sensor_read_value(int _add_, int _rec_byte_val) {
   Wire.requestFrom(_add_, _rec_byte_val, true);  // request 6 bytes from slave device #8
   while (Wire.available()) {                     // slave may send less than requested
-    for (int i = 0; i < 15; i++) {
-      c[i] = Wire.read();  // receive a byte as character
+    for (int i = 0; i < _rec_byte_val; i++) {
+      temp_char = Wire.read();
+      if(i<15)
+        c[i] =temp_char;   // receive a byte as character
       //Serial.println(c);         // print the character
     }
   }
 }
 
 uint8_t parsing_val(int sensor_no) {
-  for (int i = 7; i < 15; i++) {
+  for (int i = 4; i < 15; i++) {
     int j;
     v[j] = c[i];
     j++;
@@ -62,11 +75,15 @@ uint8_t parsing_val(int sensor_no) {
   }
 }
 
-uint8_t val_comparsion(int Sensor_old_value, int Sensor_new_value)
+bool val_comparsion(int Sensor_old_value, int Sensor_new_value)
 {
   if (Sensor_old_value != Sensor_new_value) {
     Sensor_old_value = Sensor_new_value;
-    Serial.println(c);
+    for (int i = 0; i < 15; i++) {
+     x[i] = c[i];
   }
-
+  Serial.println(x);
+  return true;
+}
+else {return false;}
 }
